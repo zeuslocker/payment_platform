292893d9ba6f549774c7c071aece49ff29aeb80d
Volodymyr Lapan <volodymyr.lapan@wework.com>
2023-06-16 11:27:30 +0200

    Add test for merchants import rake task

--- spec/lib/tasks/merchants_spec.rb
+++ spec/lib/tasks/merchants_spec.rb
@@ -6,6 +6,7 @@ Rails.application.load_tasks
 RSpec.describe 'merchants.rake' do
   let(:csv_file) { "#{fixture_path}/csv/merchants.csv" }
   it 'imports merchants' do
-    expect { Rake::Task['merchants:import'].invoke(csv_file) }.to increase { Merchant.count }.by(2)
+    expect { Rake::Task['merchants:import'].invoke(csv_file) }.to change { Merchant.count }.by(2)
+    expect(Merchant.find_by(email: 'merch1@gmail.com')).to be_truthy
   end
 end
